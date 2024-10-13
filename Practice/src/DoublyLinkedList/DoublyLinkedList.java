a78cb9419297097e6732de8252bdec08761cf13e
Edujesseyang <edujesseyang@gmail.com>
2024-10-13 11:45:40 -0700

    add

--- Practice/src/DoublyLinkedList/DoublyLinkedList.java
+++ Practice/src/DoublyLinkedList/DoublyLinkedList.java
@@ -62,68 +62,168 @@ public class DoublyLinkedList<T> {
     }
 
     public void add(T data) {
+        // create a new node with input data
         Node<T> newNode = new Node<>(data);
+        // handle the case if the list is empty
         if (this.numOfEntries == 0) {
             head = newNode;
-        } else {
+        } else { // handle the case if the list is not empty
             tail.setNext(newNode);
             newNode.setPrevious(tail);
         }
+        // update tail
         tail = newNode;
+        // update num of entries
         numOfEntries++;
     }
 
     public void add(int index, T data) {
+        // throw exception if index out of bounds
         if (index < 0 || index > numOfEntries) {
             throw new IndexOutOfBoundsException("Out of bounds!");
         }
+        // create a new node
         Node<T> newNode = new Node<T>(data);
+        // handle if add to the last
         if (index == numOfEntries) {
             tail.setNext(newNode);
             newNode.setPrevious(tail);
             tail = newNode;
-        } else if (index == 0) {
+        } else if (index == 0) { // handle if add to the first
             head.setPrevious(newNode);
             newNode.setNext(head);
             head = newNode;
-        } else {
+        } else { // adding to anywhere else than first and last
+            // handle if adding to the first half
             if (index <= numOfEntries / 2) {
+                // define current
                 Node<T> currentNode = head;
-                for (int i = 0; i < index; i++) {
+                // traversing to the index
+                for (int i = 0; i < index - 1; i++) {
                     currentNode = currentNode.getNext();
                 }
+                // define nextNode
                 Node<T> nextNode = currentNode.getNext();
+                // link new note to current and nextNode
                 newNode.setPrevious(currentNode);
                 newNode.setNext(nextNode);
+                // link both ends to the new node
                 nextNode.setPrevious(newNode);
                 currentNode.setNext(newNode);
-            } else {
+            } else { // handle if index is in the last half
+                // define current as tail
                 Node<T> currentNode = tail;
-                for (int i = 0; i < numOfEntries - index; i++) {
+                // traversing from the tail to index
+                for (int i = 0; i < numOfEntries - index - 1; i++) {
                     currentNode = currentNode.getPrevious();
                 }
+                // define pre of current
                 Node<T> preNode = currentNode.getPrevious();
+                // link new node to both sides
                 newNode.setPrevious(preNode);
                 newNode.setNext(currentNode);
+                // link both sides to the new node
                 currentNode.setPrevious(newNode);
                 preNode.setNext(newNode);
             }
         }
+        // update the num of entries
         numOfEntries++;
+    }
+
+    public void remove(int index) {
+        // throw exception if index out of bounds
+        if (index < 0 || index >= numOfEntries) {
+            throw new IndexOutOfBoundsException("Index Out of Bounds");
+        }
+
+        // handle the case that removing the first entry
+        if (index == 0) {
+            // handle the case that there is only one entry
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                // define the new head
+                Node<T> first = head.getNext();
+                // disconnect new head's pre
+                first.setPrevious(null);
+                // disconnect old head's next
+                head.setNext(null);
+                // set head is new head
+                head = first;
+            }
+            // update num of entries
+            numOfEntries--;
+            // return after the job done
+            return;
+        }
+
+        // handle the case that removing last entry
+        if (index == numOfEntries - 1) {
+            // define the new tail
+            Node<T> last = tail.getPrevious();
+            // disconnect new tail's next
+            last.setNext(null);
+            // disconnect old tail's pre
+            tail.setPrevious(null);
+            // set tail is new tail
+            tail = last;
+            // update num of entries
+            numOfEntries--;
+            // return after the job done
+            return;
+        }
+
+        // define 3 nodes,
+        Node<T> current;
+        Node<T> before;
+        Node<T> after;
+        // handle the case if index is in first half
+        if (index < numOfEntries / 2) {
+            // traverse from the head
+            current = head;
+            // traverse to one node before the one needs to be removed
+            for (int i = 0; i < index; i++) {
+                current = current.getNext();
+            }
+        } else { // handle the case if the removing node is in the last half
+            // traverse from the tail
+            current = tail;
+            // traverse to the node after the one needs to be removed
+            for (int i = 0; i < numOfEntries - index - 1; i++) {
+                current = current.getPrevious();
+            }
+        }
+        // set before and current
+        before = current.getPrevious();
+        after = current.getNext();
+
+        // connecting before to after
+        before.setNext(after);
+        after.setPrevious(before);
+        // disconnect current both side
+        current.setNext(null);
+        current.setPrevious(null);
+
+        // update num of entries
+        numOfEntries--;
     }
 
     @Override
     public String toString() {
-
-        StringBuilder result = new StringBuilder("DoublyLinkedList{head=" + head + ", tail=" + tail + ", numOfEntries={");
-
+        StringBuilder result = new StringBuilder("{");
         Node<T> currentNode = head;
         for (int i = 0; i < numOfEntries; i++) {
-            result.append(currentNode.getData().toString());
-            if (i < numOfEntries - 1) { // Add a separator for all but the last element
-                result.append(", ");
+            if (currentNode != null) {
+                result.append(currentNode.getData().toString());
+                if (i < numOfEntries - 1) { // Add a separator for all but the last element
+                    result.append(", ");
+                }
+                currentNode = currentNode.getNext();
+            } else {
+                result.append("null, ");
             }
-            currentNode = currentNode.getNext();
         }
 
         result.append("}");
--- Practice/src/DoublyLinkedList/test.java
+++ Practice/src/DoublyLinkedList/test.java
@@ -3,36 +3,106 @@ package DoublyLinkedList;
 
 public class test {
     public static void main(String[] args) {
+        System.out.println("************************* Testing adding ****************************");
         DoublyLinkedList<String> myList = new DoublyLinkedList<String>();
+        myList.add("0");
         myList.add("1");
         myList.add("2");
         myList.add("3");
         myList.add("5");
         myList.add("6");
-        System.out.println(myList);
+
+        System.out.println("Target list:\n" + myList + "\t\t(Missing 4)");
         myList.add(4, "4");
-        System.out.println(myList);
+        System.out.println("After adding to the missing index:\n" + myList);
 
         DoublyLinkedList<String> myList2 = new DoublyLinkedList<String>();
+        myList2.add("0");
         myList2.add("2");
         myList2.add("3");
         myList2.add("4");
         myList2.add("5");
         myList2.add("6");
-        System.out.println(myList2);
+        System.out.println("Target list:\n" + myList2 + "\t\t(Missing 1)");
         myList2.add(1, "1");
-        System.out.println(myList2);
+        System.out.println("After adding to the missing index:\n" + myList2);
 
         DoublyLinkedList<String> myList3 = new DoublyLinkedList<String>();
+        myList3.add("0");
         myList3.add("1");
         myList3.add("2");
         myList3.add("3");
         myList3.add("4");
         myList3.add("5");
         myList3.add("6");
-        System.out.println(myList3);
+        System.out.println("Target list:\n" + myList3 + "\t\t(Missing 7)");
         myList3.add(7, "7");
-        System.out.println(myList3);
+        System.out.println("After adding to the missing index:\n" + myList3);
+
+        DoublyLinkedList<String> myList4 = new DoublyLinkedList<String>();
+        myList4.add("0");
+        myList4.add("1");
+        myList4.add("3");
+        myList4.add("4");
+        myList4.add("5");
+        myList4.add("6");
+        myList4.add("7");
+        System.out.println("Target list:\n" + myList4 + "\t\t(Missing 2)");
+        myList4.add(2, "2");
+        System.out.println("After adding to the missing index:\n" + myList4);
+
+        System.out.println("\n******************** Test remove ******************************");
+        DoublyLinkedList<String> myList5 = new DoublyLinkedList<String>();
+        myList5.add("0");
+        myList5.add("1");
+        myList5.add("2");
+        myList5.add("3");
+        myList5.add("4");
+        myList5.add("5");
+        myList5.add("6");
+        myList5.add("7");
+        System.out.println("Old list: " + myList5);
+        myList5.remove(0);
+        System.out.println("After removed 0 : " + myList5);
+
+        DoublyLinkedList<String> myList6 = new DoublyLinkedList<String>();
+        myList6.add("0");
+        myList6.add("1");
+        myList6.add("2");
+        myList6.add("3");
+        myList6.add("4");
+        myList6.add("5");
+        myList6.add("6");
+        myList6.add("7");
+        System.out.println("Old list: " + myList6);
+        myList6.remove(7);
+        System.out.println("After removed 7 : " + myList6);
+
+        DoublyLinkedList<String> myList7 = new DoublyLinkedList<String>();
+        myList7.add("0");
+        myList7.add("1");
+        myList7.add("2");
+        myList7.add("3");
+        myList7.add("4");
+        myList7.add("5");
+        myList7.add("6");
+        myList7.add("7");
+        System.out.println("Old list: " + myList7);
+        myList7.remove(2);
+        System.out.println("After removed 2 : " + myList7);
+
+        DoublyLinkedList<String> myList8 = new DoublyLinkedList<String>();
+        myList8.add("0");
+        myList8.add("1");
+        myList8.add("2");
+        myList8.add("3");
+        myList8.add("4");
+        myList8.add("5");
+        myList8.add("6");
+        myList8.add("7");
+        System.out.println("Old list: " + myList8);
+        myList8.remove(5);
+        System.out.println("After removed 5 : " + myList8);
 
     }
 }
